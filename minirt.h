605af923a4c3623381c83a9a9b814a61e268322a
Maulken <138665211+Maulken@users.noreply.github.com>
2024-07-31 16:01:45 +0200

    at the beginning

---
+++ minirt.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmassoli <vmassoli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 13:31:23 by vmassoli          #+#    #+#             */
+/*   Updated: 2024/07/31 09:49:07 by vmassoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <string.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <fcntl.h>
+# include <math.h>
+//# include "mlx_linux/mlx.h"
+//# include "mlx_linux/mlx_int.h"
+
+# define OK 0
+# define ERROR -1
+
+#endif
