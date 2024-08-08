fbfb332b7302542c8f25b261d57639440f6e4894
ManonPe <73585054+ManonPe@users.noreply.github.com>
2024-08-08 15:57:47 +0200

    test debut pour voir

--- Makefile
+++ Makefile
@@ -3,10 +3,48 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: vmassoli <vmassoli@student.42.fr>          +#+  +:+       +#+         #
+#    By: mpelluet <mpelluet@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/07/30 13:30:15 by vmassoli          #+#    #+#              #
-#    Updated: 2024/07/30 13:30:16 by vmassoli         ###   ########.fr        #
+#    Created: 2024/01/11 11:32:32 by mpelluet          #+#    #+#              #
+#    Updated: 2024/08/08 12:29:12 by mpelluet         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+SRCS =	render\
+		
+
+INC_FILES = -Iminilibx
+
+INC_ARCHIVES = minilibx/libmlx_Linux.a
+
+LINKS = -L minilibx/ -lmlx -lXext -lX11
+
+SRC = $(addsuffix .c,$(SRCS))
+OBJ = $(addsuffix .o,$(SRCS))
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -Ofast
+
+NAME = minirt
+
+all: $(NAME)
+
+$(NAME) : $(OBJ)
+	make -C minilibx
+	$(CC) -o $@ $(OBJ) $(INC_ARCHIVES) $(LINKS)
+
+%.o: %.c
+	$(CC) -c -o $@ $< $(CFLAGS) $(INC_FILES)
+
+bonus : all
+
+clean :
+	rm -rf $(OBJ) $(BOBJ)
+
+fclean : clean
+	rm -rf $(NAME)
+
+re : fclean
+	make
+
+.PHONY : all bonus clean fclean re
--- minirt.h
+++ minirt.h
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vmassoli <vmassoli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mpelluet <mpelluet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 13:31:23 by vmassoli          #+#    #+#             */
-/*   Updated: 2024/07/31 09:49:07 by vmassoli         ###   ########.fr       */
+/*   Updated: 2024/08/08 15:33:33 by mpelluet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
+
+# include "mlx.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -24,10 +26,61 @@
 # include <X11/keysym.h>
 # include <fcntl.h>
 # include <math.h>
-//# include "mlx_linux/mlx.h"
-//# include "mlx_linux/mlx_int.h"
 
 # define OK 0
 # define ERROR -1
+
+
+typedef struct s_point
+{
+	float	x;
+	float	y;
+	float	z;
+} t_point;
+
+typedef struct s_colors
+{
+	int	r;
+	int	g;
+	int	b;
+} t_colors;
+
+typedef struct s_sphere
+{
+	t_point		*sp_point;
+	float		sp_diam;
+	t_colors	*sp_colors;
+} t_sphere;
+
+typedef struct s_data // data principal . pour la mlx
+{
+    int width;
+    int height;
+
+    double  Cw;
+    double  Ch;
+    int x;
+    int y;
+    double  t_min;
+    double  t_max;
+    double	t[2];
+    
+    void *mlx;
+    void *win;
+    void *img;
+    void *addr;
+    int		bits_per_pixel;
+    int		line_length;
+	int		endin;
+    t_point *camera;
+    t_point *D;
+    t_sphere    *sphere;
+} t_data ;
+
+
+
+
+int	ft_close(t_data *data);
+int	init_struct(t_data *data);
 
 #endif
